a6c334292ebba5f697f4cf562317dde24751facb
eebowen <nbzbws@gmail.com>
2018-12-21 20:43:05 -0600

    finished No.27

---
+++ no27RemoveElement/no27RemoveElement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
